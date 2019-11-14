52cb1f3b16c515c06822fb075a6038a967889c7e
Bryan Rhodunda <rhodunda@udel.edu>
2019-11-14 21:42:46 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -51,7 +51,7 @@ def array_literal_matrix
 end
 
 def matrix_lookup(matrix, row, column)
-  # Return the matrix's content at row and column
+  
 end
 
 def matrix_update(matrix, row, column, new_value)
