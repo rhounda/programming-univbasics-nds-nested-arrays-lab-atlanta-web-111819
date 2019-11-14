41e7a711dc36442547b746bd661127c53688d5d1
Bryan Rhodunda <rhodunda@udel.edu>
2019-11-14 21:45:41 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -1,7 +1,11 @@
 # Examples inspired by David Foster Wallace's # (2/21/1962 to 9/12/2008) _Infinite Jest_
 
 ENNET_HOUSE = [
-  
+  "Don Gately",
+  "Joelle van Dyne",
+  "Pat M.",
+  "Kate Gompert",
+  "Bruce Green"
 ]
 
 ENFIELD_TENNIS_ACADEMY = [
